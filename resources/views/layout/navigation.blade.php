0fa198c507b62f9e6d0f2020fd63a0e2cb654683
Ukindu Jr <umabruki@gmail.com>
2018-01-30 16:31:07 +0300

    Update navigation.blade.php

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -24,6 +24,10 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ url('todo/upload') }}"> Profile </a>
             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="{{ url('todo/upload') }}"> UkinduJr </a>
+            </li>
+
 
         </ul>
         <form class="form-inline my-2 my-lg-0">
